093a2d11a06f718217ca7b71b1135e2234294141
Laurence Sebastian Bowes <github@supertrianguloid.com>
2022-08-14 16:51:12 +0100

    Start work on analysis code

---
+++ src/analysis.jl
@@ -0,0 +1,11 @@
+using DrWatson
+@quickactivate "RipRep"
+include(srcdir("parser.jl"))
+
+function plot_plaquette(data)
+    
+end
+
+function fit_cosh(data, correlator, trange, thermalisation, binsize, nstates, bs, mode)
+    
+end
