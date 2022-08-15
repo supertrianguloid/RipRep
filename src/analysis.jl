abe24af80a18643d673d650248509be382b06ad9
Laurence Sebastian Bowes <github@supertrianguloid.com>
2022-08-15 12:21:47 +0100

    Add Statistics package, plot correlator with errors

--- src/analysis.jl
+++ src/analysis.jl
@@ -1,5 +1,6 @@
 using DrWatson
 using Plots
+using Statistics
 theme(:dracula)
 
 @quickactivate "RipRep"
@@ -25,6 +26,11 @@ end
 function _plot_plaquette(run_no, data)
     plot(1:nrow(data), data[:, :plaquette], label="Plaquette", title = GLOBAL_SIMS[run_no, :path])
     xlabel!("Conf #")
+end
+
+function plot_correlator(correlator)
+    plot(0:length(mean(correlator)) - 1, parent(mean(correlator)), yerr = parent(std(correlator, corrected=true)/sqrt(length(correlator))))
+    xlabel!("Imaginary Time")
 end
 
 function fit_cosh(data, correlator, trange, thermalisation, binsize, nstates, bs, mode)
