9a5d50eeffa18423188086bba53b9d8b5ef7df80
Laurence Sebastian Bowes <github@supertrianguloid.com>
2022-08-27 17:46:20 +0100

    Parse the WF metadata

--- src/analysis.jl
+++ src/analysis.jl
@@ -12,17 +12,36 @@ function acceptance(ens)
     return sum(ens.data[:, :accepted])/nrow(ens.data)
 end
 
-function thermalise(ens, ntherm)
-    ens.data = ens.data[ntherm:end, :]
+function thermalise(data, ntherm)
+    data = data[ntherm:end, :]
 end
 
-function bin(data, binsize, method)
+function bin(data, binsize, method = :equal)
+    if binsize == 1
+        return data
+    end
     offset = nrow(data) % binsize
     data = thermalise(data, offset)
     newlen = nrow(data) ÷ binsize
     if method == :equal
         return data[[1 + (i - 1)*binsize for i in 1:newlen], :]
     end
+end
+
+function _bootstrap(data, n = 1000)
+    l = length(data)
+    bs = []
+    for i ∈ 1:n
+        push!(bs, mean(data[rand(1:l, l)]))
+    end
+    return std(bs)
+end
+
+function plot_t2e(wf, binsize = 1)
+    data = bin(wf.data, binsize)
+    plot(data[1, :t], mean(data[:, :t²E]), yerr = _bootstrap(data[:, :t²E]))
+    xlabel!("\$t\$")
+    ylabel!("\$t^2E\$")
 end
 
 function plot_plaquette(ens, range = :default)
--- src/parser.jl
+++ src/parser.jl
@@ -12,6 +12,7 @@ FLOAT_REGEX = r"([-+]?[0-9]*\.?[0-9]*)"
 INTEGER_REGEX = r"([0-9]+)"
 TIME_REGEX = r"Trajectory #" * INTEGER_REGEX * r": generated in \[" * INTEGER_REGEX * " sec " * INTEGER_REGEX * r" usec\]"
 CONF_REGEX = r"\[DeltaS = " * FLOAT_REGEX_FULL * r"\]\[" * r"exp\(-DS\) = " * FLOAT_REGEX_FULL * r"\]"
+WF_REGEX = r"Configuration from .*n" * INTEGER_REGEX
 
 GLOBAL_SIMS = DataFrame(β=Float64[],
     Cˢʷ=Float64[],
@@ -32,6 +33,11 @@ GLOBAL_WF = DataFrame(β=Float64[],
 struct Ensemble
     global_metadata::DataFrame
     run_metadata::DataFrame
+    data::DataFrame
+end
+
+struct WilsonFlow
+    metadata::DataFrame
     data::DataFrame
 end
 
@@ -83,24 +89,50 @@ function load_runs(ens_no)
 end
 
 function load_wilson_flow(wf_no)
-    output = read(wf_file_location(wf_no), String)
-
-    raw_df = _convert_outformat_to_dataframe(output)
+    output = split(read(wf_file_location(wf_no), String), '\n')
     
-    data = DataFrame([Int64[], Float64[], Float64[], Float64[], Float64[], Float64[], Float64[], Float64[], Float64[]], [:conf_no, :time, :plaquette, :t, :E, :t²E, :Esym, :t²Esym, :TC])
+    data = DataFrame([Int64[], Float64[], Float64[], Vector{Float64}[], Vector{Float64}[], Vector{Float64}[], Vector{Float64}[], Vector{Float64}[], Vector{Float64}[]], [:conf_no, :time, :plaquette, :t, :E, :t²E, :Esym, :t²Esym, :TC])
 
     outer = []
     inner = []
 
-    for i in process[:, :output]
-        if(match(CONF_REGEX, i) !== nothing)
+    for i in output
+        if(match(WF_REGEX, i) !== nothing)
             push!(outer, inner)
             inner = []
         end
         push!(inner, i)
     end
     push!(outer, inner)
-    
+    outer = join.(outer, '\n')
+
+    outer = _convert_outformat_to_dataframe.(outer)
+
+    metadata = _extract_wf_metadata(outer[1])
+
+    for conf in outer[2:end]
+        conf_no = parse(Int, only(match(WF_REGEX, only(_extract_output(conf, "MAIN"))).captures))
+        time = match(r"done \[" * INTEGER_REGEX * " sec " * INTEGER_REGEX * r" usec\]", only(_extract_output(outer[2], "TIMING"))).captures
+        time = parse(Int, time[1]) + 1e-6*parse(Int, time[2])
+        plaquette = parse(Float64, match("Plaquette="*FLOAT_REGEX_FULL, _extract_output(conf, "IO")[2]).captures[1])
+        wf = []
+        for flow in _extract_output(conf, "WILSONFLOW")
+            a = match(r"WF \(t,E,t2\*E,Esym,t2\*Esym,TC\) = (.*)", flow)
+            push!(wf, parse.(Float64, split(a.captures[1], ' ')))
+        end
+        wf = hcat(wf...)'
+        push!(data, (conf_no = conf_no,
+                    time = time,
+                    plaquette = plaquette,
+                    t = wf[:, 1],
+                    E = wf[:, 2],
+                    t²E = wf[:, 3],
+                    Esym = wf[:, 4],
+                    t²Esym = wf[:, 5], 
+                    TC = wf[:, 6]
+                    ))
+    end                 
+    return WilsonFlow(metadata, data)
 end
 
 function parse_output_file(ensemble_no)
@@ -217,9 +249,27 @@ function _extract_output(proc, name)
     return p == String[] ? nothing : p
 end
 
-function extract_global_metadata(first_run::DataFrame)
-    ranlux = only(match(r"^RLXD (.+)", only(first_run[first_run.name.=="SETUP_RANDOM", :output])).captures)
+function extract_global_metadata(first_frame::DataFrame)
+    ranlux = only(match(r"^RLXD (.+)", only(_extract_output(first_frame, "SETUP_RANDOM"))).captures)
     return DataFrame(ranlux=ranlux)
+end
+
+function _extract_wf_metadata(first_frame::DataFrame)
+    ranlux = _only_match(r"^RLXD (.+)$", _extract_output(first_frame, "SETUP_RANDOM"))
+    integrator = _only_match(r"WF integrator: (.+)$", _extract_output(first_frame, "MAIN"))
+    tmax = parse(Float64, _only_match(r"tmax: (.+)$", _extract_output(first_frame, "MAIN")))
+    N_meas = parse(Int64, _only_match(r"WF number of measures: (.+)$", _extract_output(first_frame, "MAIN")))
+    ϵ₀ = parse(Float64, _only_match(r"WF initial epsilon: (.+)$", _extract_output(first_frame, "MAIN")))
+    Δ = parse(Float64, _only_match(r"WF delta: (.+)$", _extract_output(first_frame, "MAIN")))
+    dt = parse(Float64, _only_match(r"WF measurement interval dt : (.+)$", _extract_output(first_frame, "MAIN")))
+
+    return DataFrame(integrator=integrator,
+                    ranlux=ranlux,
+                    tmax=tmax,
+                    N_meas=N_meas,
+                    ϵ₀=ϵ₀,
+                    Δ=Δ,
+                    dt=dt)
 end
 
 
@@ -311,5 +361,9 @@ function summarise_ensembles(method = :show)
     return df
 end
 
+function _only_match(regex, output)
+    m = match.(regex, output)
+    return only(only(m[m .!= nothing]).captures)
+end
 
 initialise()
