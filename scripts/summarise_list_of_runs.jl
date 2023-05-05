ced2b1d2cd6741775778c1106f0515aab2cf968f
Laurence Sebastian Bowes <github@supertrianguloid.com>
2023-05-05 12:51:20 +0100

    Add new script

---
+++ scripts/summarise_list_of_runs.jl
@@ -0,0 +1,11 @@
+#!/usr/bin/julia
+include(Base.source_dir()*"/../src/parser.jl")
+using CSV
+
+ensembles = []
+
+for line in readlines(ARGS[1])
+    push!(ensembles, only(load_ensemble(line).global_metadata))
+end
+
+CSV.write(ARGS[2], DataFrame(ensembles))
