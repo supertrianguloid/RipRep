6f1a9a7226a4795036a62343246085f0b930db6d
Laurence Sebastian Bowes <github@supertrianguloid.com>
2023-06-16 13:33:00 +0100

    Improve UI

--- scripts/summarise_list_of_runs_yaml.jl
+++ scripts/summarise_list_of_runs_yaml.jl
@@ -25,29 +25,37 @@ for line in readlines(ARGS[1])
         corrs = [:g5_folded, :gk_folded, :id_folded]
 	thermalise!(ens, THERM)
         try
+            @info "PCAC mass..."
             ensembles[line][:pcac] = bootstrap_effective_pcac(ens.analysis, BINSIZE)
             plot_pcac_mass(ens, BINSIZE)
             save_figure("pcac_mass.pdf")
         catch e
+            @error "Failed!"
         end
         try
+            @info "Fps..."
             ensembles[line][:fps] = bootstrap_effective_fps(ens.analysis, ens.global_metadata[:geometry][0], ens.global_metadata[:geometry][1], BINSIZE)
             plot_fps(ens, BINSIZE)
             save_figure("fps.pdf")
         catch e
+            @error "Failed!"
         end
         try
+            @info "Gps..."
             ensembles[line][:gps] = bootstrap_effective_gps(ens.analysis, ens.global_metadata[:geometry][1], BINSIZE)
             plot_gps(ens, BINSIZE)
             save_figure("gps.pdf")
         catch e
+            @error "Failed!"
         end
         for corr in corrs
             try
+                @info String(corr) * "..."
                 ensembles[line][corr] = bootstrap_effective_mass(ens.analysis, corr, BINSIZE)
                 plot_effective_mass(ens, corr, BINSIZE)
                 save_figure("effective_mass_" * String(corr) * ".pdf")
             catch e
+                @error "Failed!"
             end
         end
     end
