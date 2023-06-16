3ba5fe2f035d8f88cf766c583d81c02a2e076256
Laurence Sebastian Bowes <github@supertrianguloid.com>
2023-06-16 13:12:04 +0100

    Fix the savefig thing

--- scripts/summarise_list_of_runs_yaml.jl
+++ scripts/summarise_list_of_runs_yaml.jl
@@ -14,6 +14,7 @@ for line in readlines(ARGS[1])
     ensemble_path = OUTPUT_DIRECTORY * replace(line, "/" => "_")[2:end] * "/"
     ensure_directory_exists(ensemble_path)
     println("Processing " * line)
+    save_figure(name) = savefig(ensemble_path * name)
     ens = load_ensemble(line)
     ensembles[line] = ens.global_metadata
     contains_nans = ens.global_metadata[:nan_confs] != []
@@ -23,26 +24,26 @@ for line in readlines(ARGS[1])
         try
             ensembles[line][:pcac] = bootstrap_effective_pcac(ens.analysis, BINSIZE)
             plot_pcac_mass(ens, BINSIZE)
-            save_figure(ensemble_path * "pcac_mass.pdf")
+            save_figure("pcac_mass.pdf")
         catch e
         end
         try
             ensembles[line][:fps] = bootstrap_effective_fps(ens.analysis, ens.global_metadata[:geometry][0], ens.global_metadata[:geometry][1], BINSIZE)
             plot_fps(ens, BINSIZE)
-            save_figure(ensemble_path * "fps.pdf")
+            save_figure("fps.pdf")
         catch e
         end
         try
             ensembles[line][:gps] = bootstrap_effective_gps(ens.analysis, ens.global_metadata[:geometry][1], BINSIZE)
             plot_gps(ens, BINSIZE)
-            save_figure(ensemble_path * "gps.pdf")
+            save_figure("gps.pdf")
         catch e
         end
         for corr in corrs
             try
                 ensembles[line][corr] = bootstrap_effective_mass(ens.analysis, corr, BINSIZE)
                 plot_effective_mass(ens, corr, BINSIZE)
-                save_figure(ensemble_path * "effective_mass_" * corr * ".pdf")
+                save_figure("effective_mass_" * corr * ".pdf")
             catch e
             end
         end
