51b85306c1b3744bb3c08c7ab3c58a67a69e34ad
Laurence Sebastian Bowes <=>
2023-06-30 17:07:11 +0100

    Try a function call

--- scripts/summarise_list_of_runs_yaml.jl
+++ scripts/summarise_list_of_runs_yaml.jl
@@ -3,6 +3,7 @@ using Base: nothing_sentinel
 using YAML
 using Dates
 using LoggingExtras
+using ThreadSafeDicts
 
 global_logger(MinLevelLogger(FileLogger(ARGS[2]), Logging.Info))
 
@@ -23,143 +24,152 @@ NO_FIT_POINTS = 4
 
 WF_REF = 1.0
 
-ensembles = Dict()
+ensembles = ThreadSafeDict()
 
 ensure_directory_exists(OUTPUT_DIRECTORY)
 
 list_of_ensembles = YAML.load_file(ARGS[1])
 
 for line in keys(list_of_ensembles)
-    ensemble_path = OUTPUT_DIRECTORY * replace(line, "/" => "_")[2:end] * "/"
-    ensure_directory_exists(ensemble_path)
-    @info "Processing " * line
-    wf = nothing
-    try
-        wf = list_of_ensembles[line]["wf"]
-        wf = load_wilsonflow(wf)
-        @info "Successfully loaded Wilson flow data"
-    catch e
-    end
+    process_ensemble(line, list_of_ensembles[line])
+end
 
-    save_figure(name) = savefig(ensemble_path * name)
-    ens = load_ensemble(line)
-    ensembles[line] = ens.global_metadata
-    contains_nans = ens.global_metadata[:nan_confs] != []
-    if ens.global_metadata[:nconfs] > 1100 && !contains_nans
-        corrs = [:g5_folded, :gk_folded, :id_folded]
-        T = ens.global_metadata[:geometry][0]
-        L = ens.global_metadata[:geometry][1]
-        T_middle = T ÷ 2
-        fit_window = (T_middle - NO_FIT_POINTS):T_middle
-	thermalise!(ens, THERM)
+
+YAML.write_file(OUTPUT_DIRECTORY * "ensembles.yml", ensembles)
+
+function process_ensemble(line, ensemble_data)
+    ensemble_path = OUTPUT_DIRECTORY * replace(line, "/" => "_")[2:end] * "/"
+    with_logger(MinLevelLogger(ensemble_path * "analysis.log", Logging.Info)) do
+        ensure_directory_exists(ensemble_path)
+        @info "Processing " * line
+        wf = nothing
         try
-            @info "Plotting plaquette..."
-            plot_plaquette(ens)
-            save_figure("plaquette.pdf")
+            wf = ensemble_data["wf"]
+            wf = load_wilsonflow(wf)
+            @info "Successfully loaded Wilson flow data"
         catch e
-            @error "Failed!"
         end
-        try
-            @info "PCAC mass..."
-            ensembles[line][:pcac] = bootstrap_effective_pcac(ens.analysis, BINSIZE)
-            plot_pcac_mass(ens, BINSIZE)
-            save_figure("pcac_mass.pdf")
-            ensembles[line][:m_pcac] = fit_pcac_mass(ens, BINSIZE, fit_window)
-            plot_pcac_fit(ens, BINSIZE, fit_window, 1:T_middle)
-            save_figure("pcac_mass_fit.pdf")
-        catch e
-            @error "Failed!"
-        end
-        try
-            @info "Fps..."
-            ensembles[line][:effective_fps] = bootstrap_effective_fps(ens.analysis, T, L, BINSIZE)
-            plot_fps(ens, BINSIZE)
-            save_figure("fps.pdf")
-            ensembles[line][:fps] = fit_fps(ens, BINSIZE, fit_window)
-            plot_fps_fit(ens, BINSIZE, fit_window, 1:T_middle)
-            save_figure("fps_fit.pdf")
-        catch e
-            @error "Failed!"
-        end
-        try
-            @info "Gps..."
-            ensembles[line][:effective_gps] = bootstrap_effective_gps(ens.analysis, ens.global_metadata[:geometry][1], BINSIZE)
-            plot_gps(ens, BINSIZE)
-            save_figure("gps.pdf")
-            ensembles[line][:gps] = fit_gps(ens, BINSIZE, fit_window)
-            plot_gps_fit(ens, BINSIZE, fit_window, 1:T_middle)
-            save_figure("gps_fit.pdf")
-        catch e
-            @error "Failed!"
-        end
-        for corr in corrs
+
+        save_figure(name) = savefig(ensemble_path * name)
+        ens = load_ensemble(line)
+        ensembles[line] = ens.global_metadata
+        contains_nans = ens.global_metadata[:nan_confs] != []
+        if ens.global_metadata[:nconfs] > 1100 && !contains_nans
+            corrs = [:g5_folded, :gk_folded, :id_folded]
+            T = ens.global_metadata[:geometry][0]
+            L = ens.global_metadata[:geometry][1]
+            T_middle = T ÷ 2
+            fit_window = (T_middle - NO_FIT_POINTS):T_middle
+            thermalise!(ens, THERM)
             try
-                @info String(corr) * "..."
-                ensembles[line][Symbol("effective_"* String(corr))] = bootstrap_effective_mass(ens.analysis, corr, BINSIZE)
-                plot_effective_mass(ens, corr, BINSIZE)
-                save_figure("effective_mass_" * String(corr) * ".pdf")
-                ensembles[line][corr] = fit_effective_mass(ens, corr, BINSIZE, fit_window)
-                plot_effective_mass_fit(ens, corr, BINSIZE, fit_window, 1:T_middle)
-                save_figure("effective_mass_" * String(corr) * "_fit.pdf")
-            catch e
-                @error "Failed!"
-            end
-        end
-        if wf != nothing
-            @info "Wilson flow..."
-            try
-                @info "t²E..."
-                plot_t2e(wf, binsize = BINSIZE)
-                save_figure("t2e.pdf")
+                @info "Plotting plaquette..."
+                plot_plaquette(ens)
+                save_figure("plaquette.pdf")
             catch e
                 @error "Failed!"
             end
             try
-                @info "W..."
-                plot_w(wf, binsize = BINSIZE)
-                save_figure("W.pdf")
+                @info "PCAC mass..."
+                ensembles[line][:pcac] = bootstrap_effective_pcac(ens.analysis, BINSIZE)
+                plot_pcac_mass(ens, BINSIZE)
+                save_figure("pcac_mass.pdf")
+                ensembles[line][:m_pcac] = fit_pcac_mass(ens, BINSIZE, fit_window)
+                plot_pcac_fit(ens, BINSIZE, fit_window, 1:T_middle)
+                save_figure("pcac_mass_fit.pdf")
             catch e
                 @error "Failed!"
             end
             try
-                @info "Topological Charge..."
-                plot_tc(wf)
-                save_figure("tc.pdf")
+                @info "Fps..."
+                ensembles[line][:effective_fps] = bootstrap_effective_fps(ens.analysis, T, L, BINSIZE)
+                plot_fps(ens, BINSIZE)
+                save_figure("fps.pdf")
+                ensembles[line][:fps] = fit_fps(ens, BINSIZE, fit_window)
+                plot_fps_fit(ens, BINSIZE, fit_window, 1:T_middle)
+                save_figure("fps_fit.pdf")
             catch e
                 @error "Failed!"
             end
             try
-                @info "Topological Charge Histogram..."
-                plot_tc_hist(wf)
-                save_figure("tc_hist.pdf")
+                @info "Gps..."
+                ensembles[line][:effective_gps] = bootstrap_effective_gps(ens.analysis, ens.global_metadata[:geometry][1], BINSIZE)
+                plot_gps(ens, BINSIZE)
+                save_figure("gps.pdf")
+                ensembles[line][:gps] = fit_gps(ens, BINSIZE, fit_window)
+                plot_gps_fit(ens, BINSIZE, fit_window, 1:T_middle)
+                save_figure("gps_fit.pdf")
+            catch e
+                @error "Failed!"
+            end
+            for corr in corrs
+                try
+                    @info String(corr) * "..."
+                    ensembles[line][Symbol("effective_"* String(corr))] = bootstrap_effective_mass(ens.analysis, corr, BINSIZE)
+                    plot_effective_mass(ens, corr, BINSIZE)
+                    save_figure("effective_mass_" * String(corr) * ".pdf")
+                    ensembles[line][corr] = fit_effective_mass(ens, corr, BINSIZE, fit_window)
+                    plot_effective_mass_fit(ens, corr, BINSIZE, fit_window, 1:T_middle)
+                    save_figure("effective_mass_" * String(corr) * "_fit.pdf")
+                catch e
+                    @error "Failed!"
+                end
+            end
+            if wf != nothing
+                @info "Wilson flow..."
+                try
+                    @info "t²E..."
+                    plot_t2e(wf, binsize = BINSIZE)
+                    save_figure("t2e.pdf")
+                catch e
+                    @error "Failed!"
+                end
+                try
+                    @info "W..."
+                    plot_w(wf, binsize = BINSIZE)
+                    save_figure("W.pdf")
+                catch e
+                    @error "Failed!"
+                end
+                try
+                    @info "Topological Charge..."
+                    plot_tc(wf)
+                    save_figure("tc.pdf")
+                catch e
+                    @error "Failed!"
+                end
+                try
+                    @info "Topological Charge Histogram..."
+                    plot_tc_hist(wf)
+                    save_figure("tc_hist.pdf")
+                catch e
+                    @error "Failed!"
+                end
+                try
+                    @info "Calculating w0..."
+                    w0 = auto_w0(wf)
+                    ensembles[line][:w0] = w0
+                catch e
+                    @error "Failed!"
+                end
+                try
+                    @info "Calculating mpiw0..."
+                    ensembles[line][:mpiw0] = propagate_product(ensembles[line][:w0], ensembles[line][:g5_folded])
+                catch e
+                    @error "Failed!"
+                end
+            end
+            try
+                @info "Calculating mpi^2..."
+                ensembles[line][:mpi2] = propagate_product(ensembles[line][:g5_folded], ensembles[line][:g5_folded])
             catch e
                 @error "Failed!"
             end
             try
-                @info "Calculating w0..."
-                w0 = auto_w0(wf)
-                ensembles[line][:w0] = w0
+                @info "Calculating mpiL..."
+                ensembles[line][:mpiL] = ensembles[line][:g5_folded][1]*L
             catch e
                 @error "Failed!"
             end
-            try
-                @info "Calculating mpiw0..."
-                ensembles[line][:mpiw0] = propagate_product(ensembles[line][:w0], ensembles[line][:g5_folded])
-            catch e
-                @error "Failed!"
-            end
-        end
-        try
-            @info "Calculating mpi^2..."
-            ensembles[line][:mpi2] = propagate_product(ensembles[line][:g5_folded], ensembles[line][:g5_folded])
-        catch e
-            @error "Failed!"
-        end
-        try
-            @info "Calculating mpiL..."
-            ensembles[line][:mpiL] = ensembles[line][:g5_folded][1]*L
-        catch e
-            @error "Failed!"
         end
         
     end
@@ -167,7 +177,5 @@ for line in keys(list_of_ensembles)
     if contains_nans
         touch(ensemble_path * "CONTAINS_NANS")
     end
+    
 end
-
-
-YAML.write_file(OUTPUT_DIRECTORY * "ensembles.yml", ensembles)
